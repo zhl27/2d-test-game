09c91c8cd8335fdd6507fe704cebec740a341ae4
Lucas <lzheng@frba.utn.edu.ar>
2025-03-17 03:10:48 -0300

    fixed

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -15,6 +15,7 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body == %Player:
 		$Timer.start()
+	print("womp womp")
 
 
 func _on_timer_timeout() -> void:
