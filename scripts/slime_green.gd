ed0968ca0418b06358a62b573fc651b25e96de5b
Lucas <lzheng@frba.utn.edu.ar>
2025-03-20 00:45:19 -0300

    slime fcking working !!!

--- scripts/slime_green.gd
+++ scripts/slime_green.gd
@@ -1,42 +1,75 @@
 extends CharacterBody2D
 
+#enum States {IDLE, RUNNING, JUMPING, FALLING, ASLEEP, JUMP_CHARGING, JUMP_READY, ATTACK_CHARGING, ATTACK_READY}
+#enum CooldownStates {CHARGING, READY}
+#
+#var current_state := States.IDLE | States.JUMP_CHARGING
+
+# 4 (100) | 2 (010) = 6 (110)
+# 0 (000) | 5 (101) = 5 # IDLE used in an operation results in the other operand state
+# 
+
 @export_category("Movement")
 @export var h_acceleration : float = 12000 # si se multiplica por delta, el valor es igual a px/segundo
 @export var jump_acceleration : float = -15000 # for jumping
 @export var horizontal_dampening : float = 0.05
 
-var do_jump : bool = false
-var do_hit : bool = false # it does not attack immediately after waking up
+var is_awake : bool
+var do_jump : bool
+var do_hit : bool 
+var players_in_damage_area : Array[Node2D]
+
+@onready var JumpCoolDown: Timer = $JumpCoolDown
+@onready var HitCoolDown: Timer = $HitCoolDown
+@onready var DamageArea: Area2D = $DamageArea
+@onready var RayCastRight: RayCast2D = $RayCastRight
+@onready var RayCastLeft: RayCast2D = $RayCastLeft
+@onready var AnimatedSprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 @export_category("Cooldowns")
-@export var jump_cooldown : float = 3 # in seconds
-@export var hit_cooldown : float = 0.5
+@export var jump_cooldown_time : float = 3 # in seconds
+@export var hit_cooldown_time : float = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$JumpCoolDown.autostart = true
-	$HitCoolDown.autostart = true
+	#print(current_state)
+	is_awake = false
+	do_hit = false # it does not attack immediately after waking up 
+	do_jump = false
+	players_in_damage_area = []
+	
+	JumpCoolDown.autostart = true
+	HitCoolDown.autostart = true
+	
+	JumpCoolDown.wait_time = jump_cooldown_time
+	HitCoolDown.wait_time = hit_cooldown_time
 	
 	add_to_group("Enemies")
 	
-	$DamageArea.monitoring = false
-	$RayCastRight.enabled = false 
-	$RayCastLeft.enabled = false
+	DamageArea.monitoring = false
+	RayCastRight.enabled = false 
+	RayCastLeft.enabled = false
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	randomize()
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor(): # if is on floor, then jump
-		velocity += get_gravity() * delta # apply gravity
-	elif do_jump:
-		jump_around(delta)
 	
-	if do_hit:
-		attack()
+	self.velocity += get_gravity() * delta # apply gravity
 	
-	velocity.x = velocity.x * (1 - horizontal_dampening) 
+	if is_awake:
+		
+		if is_on_floor() and do_jump:
+			jump_around(delta)
+			do_jump = false
+			self.JumpCoolDown.start()
+		
+		if do_hit:
+			attack()
+			do_hit = false
+			self.HitCoolDown.start()
+		
+		self.velocity.x = velocity.x * (1 - horizontal_dampening) 
 	
 	move_and_slide()
 
@@ -44,29 +77,36 @@ func _physics_process(delta: float) -> void:
 #region Behaviour
 
 func attack(): # pick one player in DamageArea and hit it
-	var player : CharacterBody2D = get_tree().get_nodes_in_group("players_still_in_area").pick_random()
-	if player: # if there is any...
-		player.hit_from(global_position)
-	do_hit = false
-	$HitCoolDown.start()
+	if players_in_damage_area.is_empty():
+		return
+		
+	players_in_damage_area.pick_random().hit_from(global_position)
 
-func jump_around(delta):	
-	do_jump = false
-	$JumpCoolDown.start()
-	if $RayCastRight.is_colliding() or $RayCastLeft.is_colliding(): # change direction when colliding
-		h_acceleration = -h_acceleration
+
+func jump_around(delta):
+	# Change direction when about to hit a wall
+	if RayCastRight.is_colliding():
+		h_acceleration = -abs(h_acceleration)  # Force negative (left)
+	elif RayCastLeft.is_colliding():
+		h_acceleration = abs(h_acceleration)   # Force positive (right)
 	else:
-		h_acceleration = randf_range(h_acceleration*0.75, h_acceleration)
+		# Add some randomness to movement
+		var direction = 1 if randf() > 0.5 else -1
+		h_acceleration = direction * h_acceleration
+	
 	velocity.x += h_acceleration * delta
 	velocity.y += jump_acceleration * delta
-
-func wake_up():
-	$DamageArea.monitoring = true
-	$AnimatedSprite2D.play("idle") 
-	$RayCastRight.enabled = true 
-	$RayCastLeft.enabled = true
 	
-	return 0 # for the await 
+	
+func wake_up():
+	DamageArea.monitoring = true
+	AnimatedSprite.play("idle") 
+	RayCastRight.enabled = true 
+	RayCastLeft.enabled = true
+	
+	is_awake = true
+	
+	return 0# for the await 
 	
 #endregion
 
@@ -74,9 +114,10 @@ func wake_up():
 #region Signal Callbacks
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	body.add_to_group("players_still_in_area")
+	if body.is_in_group("Players"):
+		players_in_damage_area.append(body)
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	body.remove_from_group("players_still_in_area")
+	players_in_damage_area.erase(body)
 
 func _on_jump_time_timeout() -> void:
 	do_jump = true
--- scripts/slime_wake_area.gd
+++ scripts/slime_wake_area.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
+@onready var AnimatedSprite: AnimatedSprite2D = $"../AnimatedSprite2D"
 
 #region Signal Callbacks
 
-func _on_body_entered(body: Node2D) -> void:
-	print("wake up !!!!")
-	$"../AnimatedSprite2D".play("wake_up")
-	await $"..".wake_up()
-	$".".queue_free()
+func _on_body_entered(_body: Node2D) -> void:
+	print(self.get_parent().name+": zzzn't")
+	AnimatedSprite.play("wake_up")
+	await self.get_parent().wake_up()
+	queue_free()
 	
 #endregion
